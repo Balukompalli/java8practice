c6b88dca0db36d194f0852b18c52f6312e3be77b
Prasad <balumail2010@gmail.com>
2023-11-22 22:09:55 +0530

    completableFuture example code and ImmutableClass

--- src/java8/Java8MainDemo.java
+++ src/java8/Java8MainDemo.java
@@ -2,6 +2,7 @@ package java8;
 
 import javax.xml.namespace.QName;
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -46,8 +47,31 @@ public class Java8MainDemo {
         List<String> upperCaseNames = stringList.stream().map(name -> name.toUpperCase()).collect(Collectors.toList());
         System.out.println("upperCaseNames names:: "+upperCaseNames);
 
-        String upperCaseNamesJoining = stringList.stream().map(name -> name.toUpperCase()).collect(Collectors.joining(",\n"));
+        String upperCaseNamesJoining = stringList.stream().map(String::toUpperCase).collect(Collectors.joining(","));
         System.out.println("upperCaseNamesJoining names:: "+upperCaseNamesJoining);
+
+        Set<String> removedDuplicateNames = employeeJava8.getEmployeeList().stream()
+                .map(EmployeeJava8::getName)
+                .sorted()
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+        System.out.println("removedDuplicateNames:: "+removedDuplicateNames);
+
+        Map<String, Long> numberOfTimeNameComes= employeeJava8.getEmployeeList().stream().map(EmployeeJava8::getName).collect(Collectors.toList())
+                .stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println("numberOfTimeNameComes: "+numberOfTimeNameComes);
+
+        List<String> onlyDuplicateNameWhichComesMoreThan1 = employeeJava8.getEmployeeList().stream().map(EmployeeJava8::getName).collect(Collectors.toList())
+                .stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().filter(entry -> entry.getValue() > 1).map(entry -> entry.getKey()).collect(Collectors.toList());
+        System.out.println(" onlyDuplicateNameWhichComesMoreThan1 :" + onlyDuplicateNameWhichComesMoreThan1);
+
+        //find it frequency
+        List<String> employeeNamesListObj = employeeJava8.getEmployeeList().stream()
+                .map(employeeJava81 -> employeeJava81.getName()).collect(Collectors.toList());
+
+        Set<String> frequencyOfNamesUsingFrequncy = employeeNamesListObj.stream()
+                .filter(name -> Collections.frequency(employeeNamesListObj, name) > 1).collect(Collectors.toSet());
+        System.out.println("frequencyOfNamesUsingFrequncy : "+frequencyOfNamesUsingFrequncy);
 
     }
 }
